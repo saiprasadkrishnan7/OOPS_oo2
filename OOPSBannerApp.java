2282e9269a792c2c5a01a6485aede118a9f4fa55
Sai Prasad <71966183+saiprasadkrishnan7@users.noreply.github.com>
2026-03-03 09:25:14 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,33 +1,23 @@
-// Create SunOfDigit Class to compute the sum of all digits of a number
-import java.util.Scanner;
-class SumOfDigits {
-   public static void main(String[] args) {
-      // Create a Scanner Object
-      Scanner input = new Scanner(System.in);
+public class step2{
+    public static void main(String[] args) {
 
-      // Get input value for number
-      int origNumber = input.nextInt();
+        String line1 = "  *****   " + "  *****   " + "  *****   " + "  *****   ";
+        String line2 = " *     *  " + " *     *  " + " *     *  " + " *        ";
+        String line3 = " *     *  " + " *     *  " + " *     *  " + " *        ";
+        String line4 = " *     *  " + " *     *  " + " * * * *  " + "  *****   ";
+        String line5 = " *     *  " + " *     *  " + " *       " + "       *  ";
+        String line6 = " *     *  " + " *     *  " + " *       " + "       *  ";
+        String line7 = "  *****   " + "  *****   " + " *       " + "  *****   ";
 
-      // Define variable number and sum initialized to zero 
-      int number = origNumber;
-      int sum = 0;
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+} 
+    
 
-      // Run while loop to access each digit of number
-      while (number != 0) {
-         // Use number % 10 to find each digit of number from last
-         int digit = number % 10;
-
-         // add each digit to sum
-         sum += digit;
-
-         // Remove last digit from number essentially get the quotient
-         number = number / 10;
-      }
-
-      // Print the sum and close the Scanner Stream
-      System.out.println("The sum of digit of number:" +origNumber+ " = " + 
-                         sum);
-      input.close();
-   }
-}
 
